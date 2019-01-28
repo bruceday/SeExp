0f2a546dc5b71f4abcbcaf8dc44c1b9e2d1170f0
bruceday <bruceday@foxmail.com>
2019-01-28 20:31:58 +0800

    1

--- SeExp/main.c
+++ SeExp/main.c
@@ -1,6 +1,48 @@
 #include<stdio.h>
-#include"dcs.h"
+//#include"dcs.h"
 #include "klee/klee.h"
+
+#define NO_FAULT 0
+#define FAULT 1
+#define FOURTH_INPUT 4
+#define THIRD_INPUT 3
+#define FIRST_INPUT 1
+#define NO_INPUT 0
+#define SECOND_INPUT 2
+#define kcg_bool int
+#define kcg_int int
+#define VALUE_1 1
+#define VALUE_0 0
+
+void FORCE_L_B_POL_TIME_INDEPENDENT_binary(
+	kcg_bool I1_status,
+	kcg_bool I1_value,
+	kcg_bool I2_status,
+	kcg_bool I2_value,
+	kcg_int SIGN,
+	kcg_bool O1_status,
+	kcg_bool O1_value
+)
+{
+	switch (SIGN)
+	{
+	case 1:
+		O1_value = I1_value;
+		O1_status = I2_status;
+		break;
+	case 2:
+		O1_value = I2_value;
+		O1_status = I1_status;
+		break;
+	case 3:
+		O1_status = I2_status;
+		O1_value = I2_value;
+	default:
+		O1_status = I1_status;
+		O1_value = I1_value;
+		break;
+	}
+}
 
 int main()
 {	
